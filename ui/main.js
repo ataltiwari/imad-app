e9c608e22f1b8a731e95880c802f28e97f37253d
ataltiwari <36676563+ataltiwari@users.noreply.github.com>
2018-03-19 20:19:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 
 
 var img=document.getElementById('atal');
+var marginLeft=0;
+function moveRight() {
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function() {
-  img.style.marginLeft='100px';
+  var interval=setInterval(moveRight,100);
 };
