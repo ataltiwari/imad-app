7e4f9e4f779ab7495255d6b5ec10b1e46fc6191f
ataltiwari <36676563+ataltiwari@users.noreply.github.com>
2018-03-19 20:01:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 
-var element=document.getElementById('html');
-html.onClick=function() {
-element.innerHTML='You are Hacked !!';
+var element=document.getElementById('img');
+img.onClick=function() {
+img.style.marginLeft='100px';
 };
