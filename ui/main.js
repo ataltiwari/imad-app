fc250d88616b2881f46a5441eb999b70cc3d5149
ataltiwari <36676563+ataltiwari@users.noreply.github.com>
2018-04-04 00:58:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,26 @@ button.onclick=function() {
     request.open('GET','http://ataltiwari96.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+//submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit-btn');
+submit.onclick=function() {
+     var request=new XMLHttpRequest();
+    request.onreadystatechange=function() {
+        if(request.readystate===XMLHttpRequest.DONE) {
+            if(request.status===200) {
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++) {
+                list+='<li>'+names[i]+'</li>';
+        }
+        var ul=document.getElementById('namelist');
+        ul.innerHTML=list;
+        }
+    }
+    };
+    request.open('GET','http://ataltiwari96.imad.hasura-app.io/submit-name?name='+name ,true);
+    request.send(null);
+};
