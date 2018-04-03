660aae7403ecf0f97418aad2bc49270875b9f8c3
ataltiwari <36676563+ataltiwari@users.noreply.github.com>
2018-04-04 01:04:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/counter', function(req,res) {
 });
 var names[];
 app.get('/submit-name',function(req,res) {
-    var names=req.query.name;
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 
